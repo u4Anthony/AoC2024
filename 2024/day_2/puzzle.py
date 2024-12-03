d3c5fb3b4a39e3eeda603af4472822217d9ea5a2
Anthony Haddad <ahaddadkc@gmail.com>
2024-12-02 23:45:47 -0500

    2024 day 2 part 2 solution

--- 2024/day_2/puzzle.py
+++ 2024/day_2/puzzle.py
@@ -4,7 +4,6 @@ total_safe_damp = 0
 def check_safety(report):
     safe = True
     index = 1
-    asc, dsc = False, False
     while index < len(report):
         if report[0] < report[1]:
             # ascending
@@ -21,31 +20,28 @@ def check_safety(report):
                 safe = False
                 break
         index += 1
-    return safe, index, asc, dsc
+    return safe, index
 
 # read the input and split it into two lists
 with open('./input.txt', 'r') as f:
     for line in f:
         report = [int(num) for num in line.split(' ')]
-        safe, index, asc, dsc = check_safety(report)
+        safe, index = check_safety(report)
         
         if safe:
             total_safe += 1
         else:
-            if asc:
-                print('ASC', report, index)
-            if index == 1:
-                del report[0]
-            else:
-                del report[index - 1]
-            safe, index, asc, dsc = check_safety(report)
-            if asc:
-                print('ASC', report, index, safe, '\r\n')
-            if safe:
-                total_safe += 1
+            for index, num in enumerate(report):
+                # exlude that index
+                sub_report = report[:index] + report[index + 1:]
+                safe, index = check_safety(sub_report)
+                if safe:
+                    total_safe_damp += 1
+                    break
+
 
 
 # puzzle one answer               
 print('Total Safe:', total_safe)
 # puzzle two answer               
-print('Total Safe with Dampener:', total_safe)
+print('Total Safe with Dampener:', total_safe + total_safe_damp)
