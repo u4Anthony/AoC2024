6894d278344e2de27cccad836934cc036f0372b7
Anthony Haddad <ahaddadkc@gmail.com>
2024-12-02 20:50:36 -0500

    day one complete

---
+++ day_1/day_one.py
@@ -0,0 +1,37 @@
+total_distance = 0
+total_similarity = 0
+list_one = []
+list_two = []
+
+# read the input and split it into two lists
+with open('./input_one.txt', 'r') as f:
+    for line in f:
+        values = ' '.join(line.split()).split(' ')
+        list_one.append(int(values[0]))
+        list_two.append(int(values[1]))
+
+# sort the lists from smallest to largest
+list_one.sort(), list_two.sort()
+
+# loop through each number and find the absolute value of distance between the two
+for index, num in enumerate(list_one):
+    total_distance += abs(num - list_two[index])
+
+# puzzle one answer
+print('Puzzle One Answer: ', total_distance)
+
+# loop through the lists and find the similarity value between matching numbers
+for num in list_one:
+    if num >= list_two[0]:
+        # count the amount of times it shows up
+        count = 0
+        for index, num_two in enumerate(list_two):
+            if num_two > num:
+                list_two = list_two[index:]
+                break
+            if num_two == num:
+                count += 1
+        total_similarity += count * num
+
+# puzzle two answer
+print('Puzzle Two Answer: ', total_similarity)
